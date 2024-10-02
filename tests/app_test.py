ef0754a74a64d7c05f777adab4cff61fd49fec76
Paarth Kashyap <kashyap.paarth@gmail.com>
2024-10-02 11:42:23 -0400

    reached end of document to follow instructions

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 import json
 from pathlib import Path
@@ -76,7 +75,7 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
-    
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get("/delete/1")
